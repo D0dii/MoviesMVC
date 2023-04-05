30aff53e6e0f6df7776a6b92fc9707530e1f3f26
marci <marci@LAPTOP-1ER67CH6>
2023-04-05 18:55:24 +0200

    Add project files.

---
+++ MoviesMVC/Data/MoviesMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MoviesMVC.Models;
+
+namespace MoviesMVC.Data
+{
+    public class MoviesMVCContext : DbContext
+    {
+        public MoviesMVCContext (DbContextOptions<MoviesMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MoviesMVC.Models.Movie> Movie { get; set; } = default!;
+    }
+}
